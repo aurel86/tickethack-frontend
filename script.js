7c8143b66c30809aeac5c188a90a1d83089b6f4f
jérôme <jplusquellec@gmail.com>
2024-07-03 16:14:59 +0200

    frontend

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+
 document.querySelector("#btn-search").addEventListener("click", function () {
-    fetch("http://localhost:3000/trips")
+    const departure = document.querySelector("#departure").value;
+    const arrival = document.querySelector("#arrival").value;
+    const date = document.querySelector("#date").value
+    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
         .then(response => response.json())
         .then(data => {
-            if (data.trip) {
-                for (let i = 0; i < data.trip.length; i++) {
+            if (data.trips) {
+                const rightSide = document.querySelector("#right-side")
+                rightSide.innerHTML += ""
+                for (let i = 0; i < data.trips.length; i++) {
+                    const depart = data.trips[i].departure
+                    const arrivee = data.trips[i].arrival
+                    const hour = new Date(data.trips[i].date).getHours()
+                    const minute = new Date(data.trips[i].date).getMinutes()
+                    
                 }
             }
         })
