870b0b8fe2cd0846a03e5c51644fe57ea4c5ec7c
jérôme <jplusquellec@gmail.com>
2024-07-03 12:57:32 +0200

    frontend

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-fetch("http://localhost:3000/Trips")
-.then(response => response.json())
-	.then(data => {
-        if (data.trip) {
-        for (let i = 0; i < data.trip.length; i++) {
-            document.querySelectorAll("#btn-search")[i].addEventListener("click", function() {
-                document.querySelectorAll("btn-search")[i]
-            })
-        }
-        }
-    })
+document.querySelector("#btn-search").addEventListener("click", function () {
+    fetch("http://localhost:3000/trips")
+        .then(response => response.json())
+        .then(data => {
+            if (data.trip) {
+                for (let i = 0; i < data.trip.length; i++) {
+                }
+            }
+        })
+})
+
